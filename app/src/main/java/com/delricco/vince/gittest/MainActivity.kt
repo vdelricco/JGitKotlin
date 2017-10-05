3e103c4eb5145822cf0472a33c3189d6d1713631
vdelricco <vd12scbc@gmail.com>
2017-10-04 21:18:23 -0600

    Create and add a file to the repo, then print the status of the repo to confirm

--- app/src/main/java/com/delricco/vince/gittest/MainActivity.kt
+++ app/src/main/java/com/delricco/vince/gittest/MainActivity.kt
@@ -3,10 +3,8 @@ package com.delricco.vince.gittest
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.Status
 import java.io.File
-
-
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,15 +18,40 @@ class MainActivity : AppCompatActivity() {
     private fun initGitRepo() {
         // prepare a new folder
         val file = File(applicationContext.filesDir, "testrepo")
-        file.delete()
 
         // create the directory
         val git = Git.init().setDirectory(file).call()
-
         println("Repository lives here: " + git.repository.directory)
         println("Current branch is: " + git.repository.branch)
 
+        val file1 = File(file, "testfile")
+        file1.writeText("Hello World!")
+        println(file1.absolutePath)
+
+        git.add().addFilepattern(".").call()
+        git.printStatusInfo()
+
         git.close()
-        file.delete()
+        file1.delete()
+        file.deleteRecursively()
+    }
+
+    fun Git.printStatusInfo() {
+        status().call().printInfo()
+    }
+
+    fun Status.printInfo() {
+        println("Added: " + added)
+        println("Changed: " + changed)
+        println("Conflicting: " + conflicting)
+        println("Conflicting State State: " + conflictingStageState)
+        println("Ignored Not In Index: " + ignoredNotInIndex)
+        println("Is Clean: " + isClean)
+        println("Missing: " + missing)
+        println("Modified: " + modified)
+        println("Removed: " + removed)
+        println("Uncommitted Changes: " + uncommittedChanges)
+        println("Untracked: " + untracked)
+        println("Untracked Folders: " + untrackedFolders)
     }
 }
